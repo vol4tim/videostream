4c9f8ba3630fe4e516f82d2d42bee009889ef5d8
vol4tim <vol4tim@gmail.com>
2023-07-04 16:20:18 +0400

    change ipfs gateway

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-  GATEWAY: "https://cloudflare-ipfs.com"
+  GATEWAY: "https://cf-ipfs.com"
   // GATEWAY: "https://ipfs-gw.decloud.foundation"
   // GATEWAY: "https://ipfs-gateway.multi-agent.io"
 };
